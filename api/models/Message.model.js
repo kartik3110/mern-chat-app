8a339490d968d74f3487495258727c077e514aab
kartik3110 <kartik31.gupta@gmail.com>
2024-02-25 21:15:24 +0530

    create message model

---
+++ api/models/Message.model.js
@@ -0,0 +1,23 @@
+import mongoose from "mongoose";
+const messageSchema = new mongoose.Schema(
+  {
+    message: {
+      type: String,
+      required: true,
+    },
+    senderId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    receiverId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+const Message = mongoose.Model("Message", messageSchema);
+export default Message;
