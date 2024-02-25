eb0a615dd87b6ee30349841f48aa9cfe81baa6bc
kartik3110 <kartik31.gupta@gmail.com>
2024-02-25 21:07:51 +0530

    created User model

---
+++ api/models/User.model.js
@@ -0,0 +1,30 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+  fullName: {
+    type: String,
+    required: true,
+  },
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  profilePic: {
+    type: String,
+    default: "https://via.placeholder.com/150",
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  gender: {
+    type: String,
+    required: true,
+    enum: ["male", "female"],
+  },
+});
+
+const User = mongoose.model("User", userSchema);
+
+export default User;
