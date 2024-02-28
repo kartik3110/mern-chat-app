d09f68876b22cb7a12a7a1d24adb739398fb3e47
kartik3110 <kartik31.gupta@gmail.com>
2024-02-29 03:01:30 +0530

    added catchAsync.js and wrapped async route handlers

--- api/routes/auth.route.js
+++ api/routes/auth.route.js
@@ -1,10 +1,11 @@
 // /api/auth/ is the base path for all the routes in the auth.route.js file.
 import express from "express";
+import catchAsync from "../utils/catchAsync.js";
 import authController from "../controllers/auth.controller.js";
 const router = express.Router();
-router.post("/signup", authController.signUp);
+router.post("/signup", catchAsync(authController.signUp));
 
-router.post("/signin", authController.signIn);
+router.post("/signin", catchAsync(authController.signIn));
 
 // According to RESTful principles, POST requests are often used for operations that have side effects, such as changing the state on the server.
 router.post("/signout", authController.signOut);
--- api/routes/message.route.js
+++ api/routes/message.route.js
@@ -1,8 +1,13 @@
 import express from "express";
 import protectRoute from "../middleware/protectRoute.js";
 import messageController from "../controllers/message.controller.js";
+import catchAsync from "../utils/catchAsync.js";
 const router = express.Router();
 
-router.post("/send/:id", protectRoute, messageController.sendMessage);
+router.post(
+  "/send/:id",
+  protectRoute,
+  catchAsync(messageController.sendMessage)
+);
 
 export default router;
