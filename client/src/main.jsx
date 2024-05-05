a71e95e6618f9bcee6aed05f302686d43bf3222d
kartik3110 <kartik31.gupta@gmail.com>
2024-05-05 21:00:32 +0530

    add redux toolkit and configure redux store

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,12 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
+import { store } from "./redux/store";
+import { Provider } from "react-redux";
 import { AuthProvider } from "./context/AuthContext.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <AuthProvider>
-      <App />
+      <Provider store={store}>
+        <App />
+      </Provider>
     </AuthProvider>
   </React.StrictMode>
 );
